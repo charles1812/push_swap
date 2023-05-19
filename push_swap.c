bc4e40a985997159650ebcbe074c1ed2b808d0a2
charles1812 <109655164+charles1812@users.noreply.github.com>
2023-05-19 22:02:15 +0200

    Add files via upload

--- push_swap.c
+++ push_swap.c
@@ -1,6 +1,54 @@
+
 #include "push_swap.h"
 #include <stdlib.h>
 #include <unistd.h>
+
+int	ft_check(struct list *list)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (i < list->len)
+	{
+		j = i + 1;
+		while(list->pA[j] != '\0' && j <= list->len)
+		{
+			if (list->pA[i] == list->pA[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	ft_real_value(struct list *list)
+{
+	int	i;
+	int	j;
+	int	tmp;
+	int	res;
+
+	i = 0;
+	while(i <= list->len)
+	{
+		tmp = list->pA[i];
+		j = 0;
+		res = 0;
+		//printf("%d\n", list->pA[i]);
+		while(j <= list->len)
+		{
+			if (list->pA[i] < list->pA[j])
+				res += 1;
+			j++;
+		}
+		list->pA[i] = list->len - res;
+		printf("%d\n\n", list->pA[i]);
+		i++;
+	}
+}
 
 void    ft_impile(int argc, char **argv, struct list *list)
 {
@@ -26,22 +74,24 @@ void    ft_impile(int argc, char **argv, struct list *list)
 int	main(int argc,char **argv)
 {
 	int		i;
-	//const int	success;
-	//const int	error;
+	char		*success;
+	const int	error;
 	struct	list *list = malloc(sizeof(struct list));
 
 	i = 0;
 	if (list == NULL)
 		return (0);
 	//success = 0;
-	//error = 84;
+	//error = "Error";
 	//list = malloc;
 	//if (argc == 2)
 	//	return (succes);
 	if (argc > 2)
 	{
 		ft_impile(argc, argv, list);
-	
+		if (ft_check(list) == 0)
+			return (error);
+		ft_real_value(list);
 		ft_rotateA(list);
 		ft_rev_rotateA(list);
 		//ft_rev_rotateA(list);
@@ -50,7 +100,7 @@ int	main(int argc,char **argv)
                         printf("%d\n", list->pA[i]);
                         i++;
                 }
-		//printf("\n\n%d\n", list->lenB);
+		printf("\n\n%d\n", ft_check(list));
 		//printf("%d", list->iB);
 		// printf("\n\n%d\n", list->lenB);
               //  printf("%d", list->iB);
